46fa165c08a3a4e35b1a0e8f11293a0372b55b7b
Margo <margarita.pristromskaya@gmail.com>
2021-03-21 15:48:26 +0300

    issues correction

--- src/test/java/StudentRegistrationFormTest.java
+++ src/test/java/StudentRegistrationFormTest.java
@@ -8,30 +8,37 @@ import static com.codeborne.selenide.Selenide.open;
 
 import java.io.File;
 
-public class StudentRegistrationForm {
+public class StudentRegistrationFormTest {
 
     @Test
     void successfulFillForm() {
-        String firstName = "Dino";
-        String lastName = "Dinosaurus";
-        String userEmail = "Dino@Dinosaurussaur.us";
-        String userNumber = "0123456789";
-        String subjectsInput = "Arts";
-        String currentAddress = "Dinopark, 12, 172";
-        String state = "Haryana";
-        String city = "Karnal";
+        String firstName = "Dino",
+                lastName = "Dinosaurus",
+                userEmail = "Dino@Dinosaurussaur.us",
+                userNumber = "0123456789",
+                monthOfBirth = "March",
+                yearOfBirth = "1965",
+                subjectsInput = "Arts",
+                currentAddress = "Dinopark, 12, 172",
+                state = "Haryana",
+                city = "Karnal";
 
         open("https://demoqa.com/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(userEmail);
         $("label[for='gender-radio-2']").click();
         $("#userNumber").setValue(userNumber);
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__day.react-datepicker__day--011").click();
+        $(".react-datepicker__month-select").selectOption(monthOfBirth);
+        $(".react-datepicker__year-select").selectOption(yearOfBirth);
+        $(".react-datepicker__day--016").click();
         $("#subjectsInput").setValue(subjectsInput).pressEnter();
         $(byText("Sports")).click();
-        $("input#uploadPicture").uploadFile(new File("src/test/resources/default_avatar.jpg"));
+      //  $("input#uploadPicture").uploadFile(new File("src/test/resources/default_avatar.jpg"));
+        $("#uploadPicture").uploadFromClasspath("default_avatar.jpg");
         $("#currentAddress").setValue(currentAddress);
         $("#react-select-3-input").setValue(state).pressEnter();
         $("#react-select-4-input").setValue(city).pressEnter();
@@ -42,12 +49,13 @@ public class StudentRegistrationForm {
                         text(userNumber),
                         text(lastName),
                         text(subjectsInput),
+                        text("Sports"),
                         text(userEmail),
                         text(currentAddress),
                         text(state),
                         text(city),
                         text("default_avatar.jpg"),
-                        text("11 March,2021")
+                        text("16 March,1965")
                 );
     }
 
